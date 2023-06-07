93d45ff8aa131e74925d0155b13c4e95f1e5f0b6
Akihiro Tojo <akihiro.021109@gmail.com>
2023-06-08 01:45:56 +0900

    Add deploy flow

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/react-ts-d3-example/',
 
   server: {
     port: 8000,
