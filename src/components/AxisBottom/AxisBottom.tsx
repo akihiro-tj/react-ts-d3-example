87243211e0a081ab5af69b3c138b0d30268e1b25
Akihiro Tojo <akihiro.021109@gmail.com>
2023-06-08 00:34:18 +0900

    Update axis style

--- src/components/AxisBottom/AxisBottom.tsx
+++ src/components/AxisBottom/AxisBottom.tsx
@@ -1,15 +1,28 @@
+import clsx from 'clsx';
 import { axisBottom } from 'd3-axis';
-import { ScaleLinear } from 'd3-scale';
+import { NumberValue, ScaleLinear } from 'd3-scale';
 import { select } from 'd3-selection';
 import { FC, useEffect, useRef } from 'react';
+
+import style from './AxisBottom.module.scss';
 
 type AxisBottom = {
   className?: string;
   y: number;
   scale: ScaleLinear<number, number, never>;
+  tickFormat?: (domainValue: NumberValue, index: number) => string;
+  label?: string;
+  labelX?: number;
 };
 
-const AxisBottom: FC<AxisBottom> = ({ className, y, scale }) => {
+const AxisBottom: FC<AxisBottom> = ({
+  className,
+  y,
+  scale,
+  tickFormat = (value: NumberValue) => value.toString(),
+  label,
+  labelX = 0,
+}) => {
   const ref = useRef<SVGGElement>(null);
 
   useEffect(() => {
@@ -17,11 +30,28 @@ const AxisBottom: FC<AxisBottom> = ({ className, y, scale }) => {
 
     const element = select(ref.current);
 
-    axisBottom(scale)(element);
-  }, [scale]);
+    axisBottom(scale).tickFormat(tickFormat)(element);
+  }, [scale, tickFormat]);
 
   return (
-    <g ref={ref} className={className} transform={`translate(0, ${y})`}></g>
+    <g
+      ref={ref}
+      className={clsx(className, style.axisBottom)}
+      transform={`translate(0, ${y})`}
+    >
+      {label && (
+        <text
+          x={labelX}
+          y={0}
+          dy="3.25em"
+          fill="#000"
+          fontSize={14}
+          textAnchor="end"
+        >
+          {label}
+        </text>
+      )}
+    </g>
   );
 };
 
