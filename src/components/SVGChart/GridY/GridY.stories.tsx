ecc1ded74844377d1e632bcbf98da3818c6a56cf
Akihiro Tojo <akihiro.021109@gmail.com>
2023-06-14 02:23:55 +0900

    Move files

--- src/components/SVGChart/GridY/GridY.stories.tsx
+++ src/components/SVGChart/GridY/GridY.stories.tsx
@@ -4,8 +4,8 @@ import { scaleLinear } from 'd3-scale';
 import { useMemo } from 'react';
 
 import useChart from '../../../hooks/useChart';
-import { Card } from '../../Card';
 import { Container } from '../../Container';
+import { Card } from '../../Layout';
 import { SVG } from '../SVG';
 
 import { GridY } from './index';
@@ -14,11 +14,19 @@ type Story = StoryObj<typeof Mock>;
 
 type MockArgs = {
   data: number[];
+  margin: {
+    top: number;
+    right: number;
+    bottom: number;
+    left: number;
+  };
+  size: {
+    width: number;
+    height: number;
+  };
 };
 
-const Mock: StoryFn<MockArgs> = ({ data }) => {
-  const { ref, size, margin } = useChart();
-
+const Mock: StoryFn<MockArgs> = ({ data, margin, size }) => {
   const scale = useMemo(() => {
     return scaleLinear()
       .domain(extent(data) as [number, number])
@@ -28,15 +36,13 @@ const Mock: StoryFn<MockArgs> = ({ data }) => {
 
   return (
     <Card>
-      <Container ref={ref}>
-        <SVG {...size}>
-          <GridY
-            minY={margin.top}
-            maxY={size.height - margin.bottom}
-            scale={scale}
-          />
-        </SVG>
-      </Container>
+      <SVG {...size}>
+        <GridY
+          minY={margin.top}
+          maxY={size.height - margin.bottom}
+          scale={scale}
+        />
+      </SVG>
     </Card>
   );
 };
@@ -44,6 +50,8 @@ const Mock: StoryFn<MockArgs> = ({ data }) => {
 export const Default: Story = {
   args: {
     data: [20, 40, 130, 55, 70],
+    margin: { top: 50, right: 45, bottom: 70, left: 60 },
+    size: { width: 600, height: 450 },
   },
 };
 
