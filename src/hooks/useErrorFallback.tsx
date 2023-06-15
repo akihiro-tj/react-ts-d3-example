8ce6b173f29011c24c662117bb5accac26300d24
Akihiro Tojo <akihiro.021109@gmail.com>
2023-06-15 18:54:46 +0900

    Fix error boundary

--- src/hooks/useErrorFallback.tsx
+++ src/hooks/useErrorFallback.tsx
@@ -2,7 +2,7 @@ import { MouseEventHandler, useCallback } from 'react';
 
 const useErrorFallback = () => {
   const onClick: MouseEventHandler<HTMLButtonElement> = useCallback(() => {
-    location.href = '/';
+    location.href = '/react-ts-d3-example/';
   }, []);
 
   return { onClick };
